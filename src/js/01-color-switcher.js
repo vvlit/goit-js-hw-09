9fe1ae6fc8aefee9ecebdbd89e63e76baa8cad2d
Valeriya Litvinchuk <43747693+vvlit@users.noreply.github.com>
2023-09-23 23:04:23 +0200

    9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let isActive = false;
+
+stopBtn.disabled = true;
+
+const changeBackgroundColor = () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const start = event => {
+    if (isActive) {
+        return;
+    };
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    isActive = true;
+
+    const timerId = setInterval(changeBackgroundColor, 1000);
+
+    stopBtn.addEventListener("click", () => {
+        clearInterval(timerId);
+        isActive = false;
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    });
+};
+
+startBtn.addEventListener("click", start);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
